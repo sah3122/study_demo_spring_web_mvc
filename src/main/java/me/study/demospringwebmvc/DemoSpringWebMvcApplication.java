103831f0114db025af531b0f7e850c8cb38044e4
dongchul <sah3122@naver.com>
2019-10-31 12:43:17 +0900

    ADD Project init

---
+++ src/main/java/me/study/demospringwebmvc/DemoSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package me.study.demospringwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringWebMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/demospringwebmvc/DemoSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.demospringwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
