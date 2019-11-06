23ac7b111ba853f4ec19662ec0261345b41a2ea0
dongchul <sah3122@naver.com>
2019-11-06 12:51:41 +0900

    ADD Handler Methods ResponseBody & ResponseEntity

--- src/main/java/me/study/demospringwebmvc/EventApi.java
+++ src/main/java/me/study/demospringwebmvc/EventApi.java
@@ -1,7 +1,9 @@
 package me.study.demospringwebmvc;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,16 +31,25 @@ public class EventApi {
      *
      * HttpEntity
      * @RequestBody와 비슷하지만 추가적으로 요청 헤더 정보를 사용 할 수 있다.
+     *
+     * ResponseEntity 는 응답 본문을 보내주기때문에 굳이 RestController 선언할 필요는 없다.
      */
     @PostMapping
     //Event 객체는 HTTPMessageConverter가 자동으로 컨버팅을 해준다.
-    public Event createEvent(@RequestBody @Valid HttpEntity<Event> request, BindingResult bindingResult) {
-        MediaType contentType = request.getHeaders().getContentType();
+    public ResponseEntity<Event> createEvent(//@RequestBody @Valid HttpEntity<Event> request,
+                                             @RequestBody @Valid Event event,
+                                             BindingResult bindingResult) {
+        //MediaType contentType = request.getHeaders().getContentType();
+
         if (bindingResult.hasErrors()) {
             bindingResult.getAllErrors().forEach(error -> {
                 System.out.println(error);
             });
+            return ResponseEntity.badRequest().build();
         }
-        return request.getBody();
+
+
+        //return ResponseEntity.ok(event);
+        return new ResponseEntity<Event>(event, HttpStatus.CREATED);
     }
 }
